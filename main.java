a4d315cb5bd24bcc1364f12c87d589f10a01be86
Enkhnasan03 <enkhush.c@gmail.com>
2025-11-12 15:07:36 +0800

    fix

--- main.java
+++ main.java
@@ -1,59 +1,93 @@
+import java.util.Date;
 public class main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
+    }
 
+}
 
 class Person{
     private String Name;
     private String SSNum;
-    private int age;
+    private int Age;
     
-    public String getName();
-    public String getSSNum();
-    public int getAge();
-    void setName(String );
-    void setSSNum();
-    void setAge();
-
-    
+    public String getName(){
+        return Name;
+    }
+    public String getSSNum(){
+        return SSNum;
+    }
+    public int getAge(){
+        return Age;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public void setSSNum(String SSNum){
+        this.SSNum = SSNum;
+    }
+    public void setAge(int Age){
+        this.Age = Age;
+    }
 }
 
-class Spouse{
-    private Date Anniversary;
-    public Date getAnniversary();
-    void getAnniversary(Date Anniversary);
-
+class Spouse extends Person{
+    private Date AnniversaryDate;
+    public Date getAnniversary(){
+        return AnniversaryDate;
+    }
+    public void setAnniversary(Date Anniversary){
+        this.AnniversaryDate = Anniversary;
+    }
 }
 class Employee{
     private String CompanyID;
     private String Title;
     private Date StartDate;
-    public String getCompanyID();
-    public String getTitle();
-    public Date getStartDate();
-    void setCompanyID(String CompanyID);
-    void setTitle(String Title);
-    void setStartDate(Date StartDate);
-
+    public String getCompanyID(){
+        return CompanyID;
+    }
+    public String getTitle(){
+        return Title;
+    }
+    public Date getStartDate(){
+        return StartDate;
+    }
+    public void setCompanyID(String CompanyID){
+        this.CompanyID = CompanyID;
+    }
+    public void setTitle(String Title){
+        this.Title = Title;
+    }
+    public void setStartDate(Date StartDate){
+        this.StartDate = StartDate;
+    }
 }
 class Child{
     private String FavoriteToy;
-    public String getFavoriteToy();
-    void setFavoriteToy(String FavoriteToy);
-
-
-
-
+    public String getFavoriteToy(){
+        return FavoriteToy;
+    }
+    public void setFavoriteToy(String FavoriteToy){
+        this.FavoriteToy = FavoriteToy;
+    }
 }
 class Division{
     private String DivisionName;
-    public String getDivisionName();
-    void setDivisionName(String DivisionName);
-
+    public String getDivisionName(){
+        return DivisionName;
+    }
+    public void setDivisionName(String DivisionName){
+        this.DivisionName = DivisionName;
+    }
 }
 class JobDescription{
     private String Description;
-    public String getDescription();
-    void setDescription(String Description);
-
-}
+    public String getDescription(){
+        return Description;
+    }
+    public void setDescription(String Description){
+        this.Description = Description;
+    }
 }
